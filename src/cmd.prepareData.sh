8f0897d83c52a5f29b0c3ca967701b02a0f5b3b8
qolina <qolina@gmail.com>
2017-08-22 21:43:42 +0800

    up

---
+++ src/cmd.prepareData.sh
@@ -0,0 +1,9 @@
+#!bin/sh
+
+#python prepareDataSet.py ../ni_data/EngMix/dev/ ../data/ACE-event-argument.txt ../ni_data/EngMix/dev.oneEventSent.txt > ../ni_data/log/out.prepDev_arg.v2 
+#python prepareDataSet.py ../ni_data/EngMix/train/ ../data/ACE-event-argument.txt ../ni_data/EngMix/train.oneEventSent.txt > ../ni_data/log/out.prepTrain_arg.v2 
+#python prepareDataSet.py ../ni_data/EngMix/test/ ../data/ACE-event-argument.txt ../ni_data/EngMix/test.oneEventSent.txt > ../ni_data/log/out.prepTest_arg.v2
+
+python prepareDataSet_joint.py ../ni_data/EngMix/dev/ ../data/ACE-event-argument.txt ../ni_data/EngMix/dev.triggerEvent.txt
+python prepareDataSet_joint.py ../ni_data/EngMix/test/ ../data/ACE-event-argument.txt ../ni_data/EngMix/test.triggerEvent.txt
+python prepareDataSet_joint.py ../ni_data/EngMix/train/ ../data/ACE-event-argument.txt ../ni_data/EngMix/train.triggerEvent.txt
---
+++ src/cmd.trigger.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#python trigger_lstm.py -train ../ni_data/pre_processed_feng/tmp.train -test ../ni_data/pre_processed_feng/tmp.test  -tag ../ni_data/pre_processed_feng/labellist -embed ../ni_data/pre_processed_feng/wordvector -vocab ../ni_data/pre_processed_feng/wordlist -model ../ni_data/models/model.trigger
+
+CUDA_VISIBLE_DEVICES=3 python trigger_lstm.py -train ../ni_data/EngMix/train.triggerEvent.txt -dev ../ni_data/EngMix/dev.triggerEvent.txt -test ../ni_data/EngMix/test.triggerEvent.txt -embed ../ni_data/ace.pretrain300.vectors -model ../ni_data/models/model.trigger
+
+#CUDA_VISIBLE_DEVICES=3 python trigger_lstm.py -train ../ni_data/EngMix/train.triggerEvent.txt -dev ../ni_data/EngMix/dev.triggerEvent.txt -test ../ni_data/EngMix/test.triggerEvent.txt -embed ../ni_data/sskip.100.vectors -model ../ni_data/models/model.trigger
